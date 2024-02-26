4403afcada3c5d238513c458bfb1a031250c1bb6
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-26 13:21:08 -0500

    Ejercicio 6 - Par o Impar

---
+++ Ejercicio 6 - Par o Impar.py
@@ -0,0 +1,9 @@
+#Algortimo para determinar si un número es par o impar
+
+num = int ( input ("Ingrese un número: "))
+
+if num % 2 == 0:
+    print ("El número es par")
+
+else:
+    print ("El número es impar")
