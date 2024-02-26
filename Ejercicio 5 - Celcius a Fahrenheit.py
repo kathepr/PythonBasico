c6827429eb0720ba5c997239724b88c149a534a8
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-26 13:15:57 -0500

    Ejercicio 5 - Celcius a Fahrenheit

---
+++ Ejercicio 5 - Celcius a Fahrenheit.py
@@ -0,0 +1,7 @@
+#Algortimo para convertir grados celsius a fahrenheit
+
+celcius = int (input("Ingrese la temperatura en grados Celcius: "))
+
+fahrenheit = (celcius*9/5) + 32
+
+print (celcius, "°C equivale a ", fahrenheit, "°F")
