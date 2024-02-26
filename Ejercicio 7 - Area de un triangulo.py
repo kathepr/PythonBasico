eba8084a39b2ac9115556515ce180861f475fe63
kathepr <160674528+kathepr@users.noreply.github.com>
2024-02-26 13:56:58 -0500

    -

--- Ejercicio 7 - Area de un triangulo.py
+++
@@ -1,8 +0,0 @@
-#Algortimo para calcular área de un triangulo 
-print ("Este es un algoritmo para calcular el área de un triangulo: \n")
-base = int (input("Ingrese la medida de la base: "))
-altura = int (input("Ingrese la medida de la altura: "))
-
-area = (altura * base)//2
-
-print ("El area del triangulo es: ", area)
